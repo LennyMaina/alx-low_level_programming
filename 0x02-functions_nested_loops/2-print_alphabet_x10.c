8e6613b9d0b71009addf40fe26f54ddb0440fa60
LennyMaina <lennymaina77@gmail.com>
2022-09-14 05:25:09 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,11 @@
  *void print_alphabet_x10 - print a to z 10 times
  *Return: always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 char c, i;
 
-for (i = '1'; i <= 10 ; ++i)
+for (i = '0'; i <= 9 ; ++i)
 {
 for (c = 'a' ; c <= 'z' ; ++c)
 putchar(c);
