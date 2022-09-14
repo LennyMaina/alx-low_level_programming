ddcf021341d9c5446e552890843ed7495c22db60
LennyMaina <lennymaina77@gmail.com>
2022-09-14 06:27:37 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *_islower - check for lower case
+ *Return: always 0
+ */
+int _islower(int c)
+{
+if(c = 'a' ;c <= z ;++c)
+printf("1");
+else
+printf("0");
+Return 0;
+}
