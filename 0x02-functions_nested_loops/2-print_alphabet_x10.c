cb26adc188a891e32a5e7a2cdb0f32ad1d355126
LennyMaina <lennymaina77@gmail.com>
2022-09-14 05:23:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/**void print_alphabet_x10 - print a to z 10 times
- * Return: always 0
+
+/**
+ *void print_alphabet_x10 - print a to z 10 times
+ *Return: always 0
  */
 void print_alphabet_x10(void)
 {
@@ -9,7 +11,7 @@ char c, i;
 for (i = '1'; i <= 10 ; ++i)
 {
 for (c = 'a' ; c <= 'z' ; ++c)
-putchar(c)
+putchar(c);
 }
 putchar('\n');
 }
