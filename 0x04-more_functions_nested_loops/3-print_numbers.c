708f8872e112f01b6b3c95f62217a3f1fab3d51d
LennyMaina <lennymaina77@gmail.com>
2022-09-15 05:32:31 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,7 @@
  *@n: input
  *Return: 0 to 9
  */
-void print_numbers(void)
+int print_numbers(void)
 {
 int n = 0;
 while (n <= 9)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -33,3 +33,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
+
+int print_numbers(void);
