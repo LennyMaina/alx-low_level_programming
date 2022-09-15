0deb85944167cbcb440ada28ee723094f5ab5001
LennyMaina <lennymaina77@gmail.com>
2022-09-15 14:17:13 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- *print_times_table - Entry point
- *@n: input
- *Return: Always 0 (Success)
+
+ * print_times_table - Entry point
+
+ * @n: input
+
+ * Return: Always 0 (Success)
+
  */
+
 void print_times_table(int n)
+
 {
-return (0);
+
+
+
+	return (0);
+
 }
