7b6efd3a923b0b60dc89641ed250302563f74fde
LennyMaina <lennymaina77@gmail.com>
2022-09-15 04:29:57 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * main - library
- * _isdigit - function to check if c is a number from 0-9
+ *_isdigit - function to check if c is a number from 0-9
  * @c: input
- * Return: 1 if true 0 if false
+ * Return: 1 if true, 0 if false
  */
-
 int _isdigit(int c)
 {
+
 if ((c >= 0) && (c <= 9))
 return (1);
 else
