7928430b461fe4004c9357f56b236e6d0eeffd0f
LennyMaina <lennymaina77@gmail.com>
2022-09-15 06:20:52 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *print_numbers -  checks for checks for a digit (0 through 9).
  *Return: Always 0.
@@ -10,6 +9,7 @@ int a;
 
 for (a = 48 ; a < 58 ; a++)
 {
+if ((i == 2) || (i == 4))
 _putchar(a);
 }
 _putchar('\n');
