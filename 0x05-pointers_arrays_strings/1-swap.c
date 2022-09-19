48bcb34900ffb5e8e4a09b76e39db98626a54bf7
LennyMaina <lennymaina77@gmail.com>
2022-09-19 02:31:29 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *swap_int - swapping
+ *@a: input values
+ *@b: input values
+ *Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+printf("Enter the values of *a and *b");
+{
+*b = *a + *b;
+*a = *b - *a;
+*b = *b - *a;
+}
+printf("%d\t%d", *a, *b);
+}
+
+
