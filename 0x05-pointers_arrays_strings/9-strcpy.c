cb7dad24a383f2442f823d48b23f0a4a3081eab3
LennyMaina <lennymaina77@gmail.com>
2022-09-19 17:26:17 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,6 @@
  * @src: destiny data
  * Return: return value of dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -18,6 +17,7 @@ char *_strcpy(char *dest, char *src)
 			*(dest + i) = *(src + i);
 		}
 	}
+
 	*(dest + i) = '\0';
 	return (dest);
 }
