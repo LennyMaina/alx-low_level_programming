742a71625c1bd4901a55cfe048c1a51710300317
LennyMaina <lennymaina77@gmail.com>
2022-10-11 06:01:31 -0700

    dog

---
+++ 100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - prints prime factors of 612852475143
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+long x, y;
+x = 612852475143;
+for (y = 2; x > y; y++)
+{
+while (x % y == 0)
+{
+x = x / y;
+}
+}
+printf("%lu", y);
+putchar('\n');
+return (0);
+}
