2972eaf397ce001b16ae404450ead6e377aa0a3a
LennyMaina <lennymaina77@gmail.com>
2022-10-28 15:04:21 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
-
 	int nodes = 0;
 
 	while (h)
 	{
-		nodes++
+		nodes++;
+
 		printf("%d", h->n);
+
 		h = h->next;
+
 		printf("\n");
 	}
-
 	return (nodes);
 }
