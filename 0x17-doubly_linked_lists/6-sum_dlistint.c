da8eb98792945c2f555eb15b4919458b80974689
LennyMaina <111064242+LennyMaina@users.noreply.github.com>
2022-12-09 02:10:23 +0300

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,12 +6,12 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int add_result = 0;
+int add_result = 0;
 
-	while (head != NULL)
-	{
-		add_result += head->n;
-		head = head->next;
-	}
+while (head != NULL)
+{
+add_result += head->n;
+head = head->next;
+}
 	return (add_result);
 }
