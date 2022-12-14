f023730521b08db2c33fa1866861105276d7f68e
LennyMaina <111064242+LennyMaina@users.noreply.github.com>
2022-12-14 05:48:45 +0300

    Create 0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isupper - Upper Alphabet
+ *
+ *@c: parameter
+ *
+ *Return: (1) , (0)
+ */
+int _isupper(int c)
+{
+		if (c >= 65 && c <= 90)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
